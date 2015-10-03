ec5c3216c96f59a755c7fb68845258e86fcb8967
lizhi <wanlizhi@outlook.com>
2015-10-03 12:29:59 +0800

    [+][CaptureHotKey] WPF 中增加了捕获热键的功能。通过windows procedure 的方式捕获。

---
+++ src/Talkie/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Talkie
+{
+    public enum KeyModifiers
+    {
+        None = 0,
+        Alt = 1,
+        Ctrl = 2,
+        Shift = 4,
+        WindowsKey = 8
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const Int32 MY_HOTKEYID = 0x9999;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            RegisterHotKey(handle, MY_HOTKEYID, (uint)KeyModifiers.Alt, 48);
+
+            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+
+            source.AddHook(WndProc);
+        }     
+
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handle)
+        {
+            if (wParam.ToInt32() == MY_HOTKEYID)
+            {
+                Console.WriteLine("he");
+                //全局快捷键要执行的命令
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
